eb14ec5c38d98f5eeee8ebe4d19cc3210b5daf33
doublethink-bps <sirase@charatech-bps.com>
2023-11-21 03:44:47 +0900

    fix comment

--- zabbixAutofix.sh
+++ zabbixAutofix.sh
@@ -5,8 +5,8 @@ result=$(python3 ./autoFix.py "$1")
 res=$?
 # If return code is 0, send result of runed the command to zabbix server.
 if [ $res = 0 ]; then
-    zabbix_sender -z 192.168.3.26 -p 10051 -s "test01" -k zabbixTrapper -o "$result"
+    zabbix_sender -z <zabbix server ip> -p 10051 -s "<target hostname>" -k zabbixTrapper -o "$result"
 # If return code is not 0, send error message to zabbix server.
 else
-    zabbix_sender -z 192.168.3.26 -p 10051 -s "test01" -k zabbixTrapper -o "Error runed script."
+    zabbix_sender -z <zabbix server ip> -p 10051 -s "<target hostname>" -k zabbixTrapper -o "Error runed script."
 fi
